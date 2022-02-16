11280e1f6bc1b4db41e44dafa7a92a936d0b3420
C Bares <colinedwinbares@gmail.com>
2022-02-16 17:51:45 -0600

    added routes for comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,14 +1,22 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('sequelize');
 
 // get all posts
 router.get('/', (req, res) => {
   console.log('==================');
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
     order: [['created_at', 'DESC']],
+    attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username'],
@@ -40,6 +48,14 @@ router.get('/:id', (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username'],
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { json } = require('express/lib/response');
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -26,6 +26,14 @@ router.get('/:id', (req, res) => {
       {
         model: Post,
         attributes: ['id', 'title', 'post_url', 'created_at'],
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
       },
       {
         model: Post,
